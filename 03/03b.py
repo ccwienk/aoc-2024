c6c4b9852d2de54280999d273565f33cbcd8f8fd
Christian Cwienk <christian.cwienk@sap.com>
2024-12-03 17:11:41 +0100

    solve day-3

--- 03/03b.py
+++ 03/03b.py
@@ -9,11 +9,22 @@ def main():
     with open(util.input_file) as f:
         i = f.read()
 
-    mulre = re.compile(r'mul\(\d+,\d+\)')
+    mulre = re.compile(r"mul\(\d+,\d+\)|don?'?t?\(\)")
 
     total = 0
+    enabled = True
 
     for m in mulre.findall(i):
+        if "don't" in m:
+            enabled = False
+            continue
+        if "do" in m:
+            enabled = True
+            continue
+
+        if not enabled:
+            continue
+
         m = m.removeprefix('mul(')
         m = m.strip(')')
         l,r = m.split(',')
