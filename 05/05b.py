615b4641a54b63aebbf612945482835a4eb07c31
Christian Cwienk <christian.cwienk@sap.com>
2024-12-05 22:52:29 +0100

    solve day-5

--- 05/05b.py
+++ 05/05b.py
@@ -34,18 +34,31 @@ def main():
             return False
         return True
 
-    ok = 0
-    total = 0
-    for update in updates:
-        if check_update(update):
-            ok += 1
-            mid = update[int(len(update)/2)]
-            total += mid
+    def fix_order(update: list[int, ...]):
+        for l,r in order_rules:
+            if not (l in update and r in update):
+                continue # rule does not apply
+            l_index = update.index(l)
+            r_index = update.index(r)
+            if l_index < r_index:
+                continue # rule is not violated
 
-    print(f'{ok=}')
+            # mv l immediately before r
+            del update[l_index]
+            update.insert(r_index, l)
+        return update # might still violate some rules
+
+    incorrect = [u for u in updates if not check_update(u)]
+
+    total = 0
+    for update in incorrect:
+        while not check_update(update):
+            fix_order(update)
+
+        total += update[int(len(update)/2)]
+
     print(f'{total=}')
 
-    print(f'{len(order_rules)=}')
 
 if __name__ == '__main__':
     main()
